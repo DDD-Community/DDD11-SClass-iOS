fbb19fbe677b8671dd55a1dae5bf121db925c65c
현수빈 <sunny5875@uniwaffle.com>
2024-08-20 23:33:20 +0900

    [Feat] #4 - ButtonSmall 컴포넌트 구현 [Feat] #4 - InputField 컴포넌트 구현 [Feat] #4 - TopArticleNavigation 컴포넌트 구현 [Feat] #4 - ColoredText 컴포넌트 구현 [Add] #4 - 아이콘 에셋 추가

--- Projects/App/Sources/Application/AppDelegate.swift
+++ Projects/App/Sources/Application/AppDelegate.swift
@@ -1,10 +1,14 @@
 import UIKit
+import SharedDesignSystem
 
 class AppDelegate: UIResponder, UIApplicationDelegate {
   func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    
+    SharedDesignSystemFontFamily.registerAllCustomFonts()
+    
     return true
   }
   
